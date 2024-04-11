166e9f8a67a4fab575bd58fe03b65f0877f08cc5
VictorMpala <apexmedia1799@gmail.com>
2024-04-11 14:32:36 +0200

    added link to the personal website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,8 +43,8 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://victormpala.github.io/">
+        Personal Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
